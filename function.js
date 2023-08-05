eaab0e2e5e5ec7e11a71151360d8c8fe98dc39e1
Muhammad Mostafizur Rahman Monir <103310114+Mustafiz1998@users.noreply.github.com>
2023-08-05 21:08:11 +0400

    more example added

--- function.js
+++ function.js
@@ -48,3 +48,67 @@ const modulus=(a)=>{
   return a%2
 }
 console.log(modulus(10));
+// function declaration:
+console.log(positveAdder([2,3,-6,12,-20]));//because of hoistiing
+function positveAdder(arr){
+    let sum = 0;
+    
+    for(let num of arr){
+     if(num>0){
+        sum+=num;
+     }
+
+    }
+    return sum;
+}
+
+//max number
+function max(arr){
+    let max=[0];
+    for(let num of arr){
+        if(num>max){
+            max=num;
+        }
+    }
+    return max;
+}
+console.log(max([1,3,4,5,6,7]));
+
+//function Expression:
+const positveAdder=function(arr){
+    let sum=0;
+    for(let num of arr){
+        if(num>0){
+            sum+=num;
+        }
+    }
+    return sum;
+}
+console.log(positveAdder([1,23,-3]));
+
+
+
+//max number
+const findMax=function(arr){
+    let max=[0];
+    for(let num of arr){
+        if(max<num){
+            max=num;
+        }
+    }
+    return max;
+}
+console.log(findMax([2,4,5,6,7]));
+
+//arrow function
+const findMin=(arr)=>{
+    let min=arr[0];
+
+    for(let num of arr){
+        if(num<min){
+            min=num;
+        }
+    }
+    return min;
+}
+console.log(findMin([2,4,5,1,0]));
