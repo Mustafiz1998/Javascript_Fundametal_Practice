00d808671a51d6afe181be9ad7cec1ef5a6c3a6c
Muhammad Mostafizur Rahman Monir <103310114+Mustafiz1998@users.noreply.github.com>
2023-08-06 16:57:02 +0400

    array first commit

---
+++ array.js
@@ -0,0 +1,9 @@
+//example-1:
+const numbers=[1,2,3];
+
+const result=[];
+
+for(let i=0; i<numbers.length; i++){
+    result.push(numbers[i]*2);
+}
+console.log(result);
---
+++ higherorderfunction.js
@@ -0,0 +1,43 @@
+// //higher order function
+// function sawMill(tree,fn){
+//     return fn(tree);
+// }
+// function treeCutter(tree){
+//     return tree*4;
+// }
+// function furnitureMaker(tree){
+//     return tree*0.1;
+// }
+
+// console.log(sawMill(255,treeCutter));
+// console.log(sawMill(255,furnitureMaker));
+
+// //callback function:
+// const callback=(n)=>{
+//     return n**2
+// }
+// function cube(callback,n){
+//     return callback(n)*n
+// }
+// console.log(cube(callback,3));
+
+//example-2:
+
+function square(x){
+    return x*2;
+}
+function higherOrderFunction(num,callback){
+    return callback(num);
+}
+// console.log(higherOrderFunction(6,square));
+
+function funName(param,fun){
+    return fun(param);
+}
+
+function mutifly(param){
+    return param*3;
+}
+console.log(funName(3,mutifly));
+
+
