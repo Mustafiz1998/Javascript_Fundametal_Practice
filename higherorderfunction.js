5e456ebc99871143f8026193630e34b1281bc7a0
Muhammad Mostafizur Rahman Monir <103310114+Mustafiz1998@users.noreply.github.com>
2023-08-06 17:04:40 +0400

    second commit

--- higherorderfunction.js
+++ higherorderfunction.js
@@ -1,35 +1,35 @@
 // //higher order function
-// function sawMill(tree,fn){
-//     return fn(tree);
-// }
-// function treeCutter(tree){
-//     return tree*4;
-// }
-// function furnitureMaker(tree){
-//     return tree*0.1;
-// }
+function sawMill(tree,fn){
+    return fn(tree);
+}
+function treeCutter(tree){
+    return tree*4;
+}
+function furnitureMaker(tree){
+    return tree*0.1;
+}
 
-// console.log(sawMill(255,treeCutter));
-// console.log(sawMill(255,furnitureMaker));
+console.log(sawMill(255,treeCutter));
+console.log(sawMill(255,furnitureMaker));
 
-// //callback function:
-// const callback=(n)=>{
-//     return n**2
-// }
-// function cube(callback,n){
-//     return callback(n)*n
-// }
-// console.log(cube(callback,3));
+// // //callback function:
+const callback=(n)=>{
+    return n**2
+}
+function cube(callback,n){
+    return callback(n)*n
+}
+console.log(cube(callback,3));
 
 //example-2:
 
-// function square(x){
-//     return x*2;
-// }
-// function higherOrderFunction(num,callback){
-//     return callback(num);
-// }
-// // console.log(higherOrderFunction(6,square));
+function square(x){
+    return x*2;
+}
+function higherOrderFunction(num,callback){
+    return callback(num);
+}
+console.log(higherOrderFunction(6,square));
 
 function funName(param,fun){
     return fun(param);
